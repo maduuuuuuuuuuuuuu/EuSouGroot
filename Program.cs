daa0a6de561a7a8e7b4d0665d3ee59dd3d2a349e
maduuuuuuuuuuuuuu <e107ti@etec.sp.gov.br>
2023-11-28 19:35:59 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string pergunta;
+const string sair = "tchau";
+
+Console.Clear();
+Console.WriteLine(" Conversa com o Groot  ");
+Console.WriteLine();
+
+do
+{
+    Console.Write("Digite sua pergunta: ");
+    pergunta = Console.ReadLine()!;
+    
+    Console.WriteLine($"                          Groot: Eu sou Groot ");
+}while (pergunta != sair);
